e14bd2a7f8acaa67da553f432785917fadf37f45
SeanDuffie <sean.duffie@tamu.edu>
2022-12-08 01:30:08 -0600

    Initial Python commit

---
+++ src/__main__.py
@@ -0,0 +1,12 @@
+"""
+This is the main file to be run for the Server Gateway
+"""
+import logging
+import threading
+
+
+if __name__ == "__main__":
+    """
+    Main function - will be run if this file is specified in terminal
+    """
+    logging.info("TODO")
